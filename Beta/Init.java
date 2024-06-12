79de307954f145d172f23ba002beb22691a57236
Arif Fikry <muhammadariffikry@graduate.utm.my>
2024-06-12 22:05:32 +0800

    update 2

--- Beta/Init.java
+++ Beta/Init.java
@@ -80,7 +80,7 @@ class Activities extends Budget {
 
     public void addActivityExpense() {
         addExpense(activityCost);
-        JOptionPane.showMessageDialog(null, "Added : " + activityName + " with cost: RM" + activityCost);
+        JOptionPane.showMessageDialog(null, "Added activity: " + activityName + " with cost: RM" + activityCost);
     }
 
     public void displayBudget() {
@@ -93,8 +93,27 @@ class Transportation extends Budget {
     private String transportType;
     private double transportCost;
 
+    // No-args Constructor
     public Transportation() {
         super();
+    }
+
+    // Type method
+    public void setName(String transportType) {
+        this.transportType = transportType;
+    }
+
+    public String getName() {
+        return transportType;
+    }
+
+    // Cost method
+    public void setCost(double transportCost) {
+        this.transportCost = transportCost;
+    }
+
+    public double getCost() {
+        return transportCost;
     }
 
     public void setTransportDetails(String transportType, double transportCost) {
@@ -104,12 +123,13 @@ class Transportation extends Budget {
 
     public void addTransportExpense() {
         addExpense(transportCost);
-        JOptionPane.showMessageDialog(null, "Added transport: " + transportType + " with cost: " + transportCost);
+        JOptionPane.showMessageDialog(null, "Added transport: " + transportType + " with cost: RM" + transportCost);
     }
 
     public void displayBudget() {
         super.displayBudget();
-        JOptionPane.showMessageDialog(null, "Transport Type: " + transportType + "\nTransport Cost: " + transportCost);
+        JOptionPane.showMessageDialog(null,
+                "Transport Type: " + transportType + "\nTransport Cost: RM" + transportCost);
     }
 }
 
@@ -117,75 +137,75 @@ public class Init {
     public static void main(String[] args) {
         Vector<Budget> tripBudgets = new Vector<>();
 
-        JFrame frame = new JFrame("Camping Trips Planner!");
-        frame.setSize(450, 450);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        frame.setResizable(false);
+        JFrame farif = new JFrame("BUdget Camping Trips Planner!");
+        farif.setSize(450, 450);
+        farif.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        farif.setLayout(null);
+        farif.setResizable(false);
 
-        JLabel header = new JLabel("Plan your trips!");
-        header.setBounds(30, 1, 200, 100);
-        header.setFont(new Font("Arial", Font.BOLD, 18));
-        frame.add(header);
+        JLabel headarif = new JLabel("Plan your trips!");
+        headarif.setBounds(30, 1, 200, 100);
+        headarif.setFont(new Font("Arial", Font.BOLD, 18));
+        farif.add(headarif);
 
-        JButton addTripButton = new JButton("Add New Trip");
-        addTripButton.setBounds(50, 150, 150, 30);
-        frame.add(addTripButton);
+        JButton addTripButtonarif = new JButton("Add New Trip");
+        addTripButtonarif.setBounds(50, 150, 150, 30);
+        farif.add(addTripButtonarif);
 
-        JButton showTripButton = new JButton("Show Trips");
-        showTripButton.setBounds(230, 150, 150, 30);
-        frame.add(showTripButton);
+        JButton showTripButtonarif = new JButton("Show Trips");
+        showTripButtonarif.setBounds(230, 150, 150, 30);
+        farif.add(showTripButtonarif);
 
-        addTripButton.addActionListener(new ActionListener() {
+        addTripButtonarif.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                header.setText("Add New Trip");
-                frame.remove(addTripButton);
-                frame.remove(showTripButton);
+                headarif.setText("Add New Trip");
+                farif.remove(addTripButtonarif);
+                farif.remove(showTripButtonarif);
 
                 JLabel labelBudget = new JLabel("Total Budget");
                 labelBudget.setBounds(30, 50, 300, 100);
-                frame.add(labelBudget);
+                farif.add(labelBudget);
 
                 JTextField budgetField = new JTextField();
                 budgetField.setBounds(30, 110, 150, 30);
-                frame.add(budgetField);
+                farif.add(budgetField);
 
                 JLabel labelActivityName = new JLabel("Activity Name");
                 labelActivityName.setBounds(30, 120, 300, 100);
-                frame.add(labelActivityName);
+                farif.add(labelActivityName);
 
                 JTextField activityNameField = new JTextField();
                 activityNameField.setBounds(30, 180, 150, 30);
-                frame.add(activityNameField);
+                farif.add(activityNameField);
 
                 JLabel labelActivityCost = new JLabel("Activity Cost");
                 labelActivityCost.setBounds(30, 190, 300, 100);
-                frame.add(labelActivityCost);
+                farif.add(labelActivityCost);
 
                 JTextField activityCostField = new JTextField();
                 activityCostField.setBounds(30, 250, 150, 30);
-                frame.add(activityCostField);
+                farif.add(activityCostField);
 
                 JLabel labelTransportType = new JLabel("Transport Type");
                 labelTransportType.setBounds(230, 50, 300, 100);
-                frame.add(labelTransportType);
+                farif.add(labelTransportType);
 
                 String[] transportOptions = { "Car", "Bus", "Train", "Miscellaneous" };
                 JComboBox<String> transportTypeComboBox = new JComboBox<>(transportOptions);
                 transportTypeComboBox.setBounds(230, 110, 150, 30);
-                frame.add(transportTypeComboBox);
+                farif.add(transportTypeComboBox);
 
                 JLabel labelTransportCost = new JLabel("Transport Cost");
                 labelTransportCost.setBounds(230, 120, 300, 100);
-                frame.add(labelTransportCost);
+                farif.add(labelTransportCost);
 
                 JTextField transportCostField = new JTextField();
                 transportCostField.setBounds(230, 180, 150, 30);
-                frame.add(transportCostField);
+                farif.add(transportCostField);
 
                 JButton saveButton = new JButton("Save Trip");
                 saveButton.setBounds(300, 340, 100, 30);
-                frame.add(saveButton);
+                farif.add(saveButton);
 
                 saveButton.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
@@ -213,14 +233,14 @@ public class Init {
                         tripBudgets.add(activities);
                         tripBudgets.add(transport);
 
-                        header.setText("Saved Trip Detail");
+                        headarif.setText("Saved Trip Detail");
 
-                        frame.remove(budgetField);
-                        frame.remove(activityNameField);
-                        frame.remove(activityCostField);
-                        frame.remove(transportTypeComboBox);
-                        frame.remove(transportCostField);
-                        frame.remove(saveButton);
+                        farif.remove(budgetField);
+                        farif.remove(activityNameField);
+                        farif.remove(activityCostField);
+                        farif.remove(transportTypeComboBox);
+                        farif.remove(transportCostField);
+                        farif.remove(saveButton);
 
                         labelBudget.setText("Total Budget: " + budget);
                         labelActivityName.setText("Activity Name: " + activityName);
@@ -228,17 +248,17 @@ public class Init {
                         labelTransportType.setText("Transport Type: " + transportType);
                         labelTransportCost.setText("Transport Cost: " + transportCost);
 
-                        frame.revalidate();
-                        frame.repaint();
+                        farif.revalidate();
+                        farif.repaint();
                     }
                 });
 
-                frame.revalidate();
-                frame.repaint();
+                farif.revalidate();
+                farif.repaint();
             }
         });
 
-        showTripButton.addActionListener(new ActionListener() {
+        showTripButtonarif.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 StringBuilder tripsDetails = new StringBuilder();
                 for (Budget budget : tripBudgets) {
@@ -253,11 +273,11 @@ public class Init {
                         budget.displayBudget();
                     }
                 }
-                JOptionPane.showMessageDialog(frame, tripsDetails.toString(), "Trips Details",
+                JOptionPane.showMessageDialog(farif, tripsDetails.toString(), "Trips Details",
                         JOptionPane.INFORMATION_MESSAGE);
             }
         });
 
-        frame.setVisible(true);
+        farif.setVisible(true);
     }
 }
